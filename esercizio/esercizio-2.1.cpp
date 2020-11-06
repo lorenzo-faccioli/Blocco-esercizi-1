c62967cd0b0d834461e311b5cc6c7cea1221cc7d
unknown <lorenzofaccio01@gmail.com>
2020-11-06 15:01:15 +0100

    programma 2.1 corretto

--- esercizio/esercizio-2.1.cpp
+++ esercizio/esercizio-2.1.cpp
@@ -1,32 +1,29 @@
-#include <iostream>
 using namespace std;
+#include <iostream>
 /* program that calculates the sum of ten values that are 
 inserted by the user
 */
 
+int somma(int a[]);
 
-
-int somma (int x[10]={});
-
-int main (){
-    int a[10]={}, valore;
-    cout<<"il programma funziona per 10 numeri";
-    for(int i=0;i<9;i++){
-        cout<<"dammi il numero";
-        cin>>a[i];
+int main()
+{
+    int a[10] = {}, valore;
+    cout << "il programma funziona per 10 numeri \n";
+    for (int i = 0; i < 10; i++) {
+        cout << "dammi il numero";
+        cin >> a[i];
     }
-    valore= somma (a);
-    cout<<"la somma  vale "<<valore;
-return 0;
+    valore = somma(a);
+    cout << "la somma  vale " << valore;
+    return 0;
 }
 
-
-int somma (int a[10]={})
+int somma(int a[10])
 {
-    int variabile=0;
-    for (int i=0;i<9;i++){
-        variabile=variabile+a[i];
+    int variabile = 0;
+    for (int i = 0; i < 10; i++) {
+        variabile = variabile + a[i];
     }
     return variabile;
-    }
-
+}

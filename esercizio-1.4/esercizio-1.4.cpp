a18b7074ddb2ffd02c602b6e954d26f38bc22242
unknown <lorenzofaccio01@gmail.com>
2020-11-21 18:04:44 +0100

    esercizio 1.4 svolto

---
+++ esercizio-1.4/esercizio-1.4.cpp
@@ -0,0 +1,18 @@
+/* program tha returns all the multiples of two 
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int val=2, multpl=1;
+    
+
+    while (1){
+        cout<<pow(val,multpl)<<endl;
+        multpl++;
+        
+    }
+
+return 0;
+}
